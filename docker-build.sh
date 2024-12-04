e5c5d1917aa84eedf4ed3c8069c75f426432773d
iamtienng <iamtienng@gmail.com>
2024-12-04 21:40:43 +0100

    feat

--- docker-build.sh
+++
@@ -1,4 +0,0 @@
-docker build -t iamtienng/ubuntu-utils . --platform linux/amd64
-docker tag iamtienng/ubuntu-utils:latest iamtienng/ubuntu-utils:v0.0.0
-docker push iamtienng/ubuntu-utils
-docker push iamtienng/ubuntu-utils:v0.0.0
